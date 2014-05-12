ddd798bf25c4fcc588ac881573fe48fd79630659
User1 <user1@Administrators-iMac-79.local>
2014-05-12 18:27:58 -0400

    github

---
+++ github.h
@@ -0,0 +1,13 @@
+//
+//  github.h
+//  githubproject
+//
+//  Created by User1 on 2014-05-12.
+//  Copyright (c) 2014 banana. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface github : NSObject
+
+@end
---
+++ github.m
@@ -0,0 +1,13 @@
+//
+//  github.m
+//  githubproject
+//
+//  Created by User1 on 2014-05-12.
+//  Copyright (c) 2014 banana. All rights reserved.
+//
+
+#import "github.h"
+
+@implementation github
+
+@end
